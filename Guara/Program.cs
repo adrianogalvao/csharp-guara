1f3516c25ffa729afc93a77ac5e9723007ab5712
Adriano Galvão <adrianogalvao@agbtecnologia.com.br>
2019-12-02 00:27:03 -0300

    Adicionar arquivos de projeto.

---
+++ Guara/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Guara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nd = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nd[0]);
+
+            int d = Convert.ToInt32(nd[1]);
+
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp));
+
+            int tam = n;
+
+            int inv = d;
+
+            int[] aux = new int[tam];
+            int k = 0;
+            int pos = inv;
+
+            for (int i = 0; i < tam; i++)
+            {
+                if (i < inv)
+                {
+                    aux[tam - pos] = (i + 1);
+                    pos--;
+                }
+                else
+                {
+                    aux[k] = a[i];
+                    k++;
+                }                
+            }
+
+            Console.WriteLine("{0}", string.Join(" ", aux));     
+        }
+    }
+}
